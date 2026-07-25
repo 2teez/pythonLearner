faa534d780810c869e54c521e0b1019b06412eae
timothy adigun <adigun.timothy@gmail.com>
2026-07-25 04:27:53 +0100

    using print in python

---
+++ junkz/hello.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+def main() -> None:
+    print("Hello, World!", flush=True)
+
+
+if __name__ == "__main__":
+    main()
---
+++ junkz/hello2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+def main() -> None:
+    print("This is what your python interpreter is doing")
+
+
+if __name__ == "__main__":
+    main()
