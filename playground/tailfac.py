4aea4bd3f287abd5baea0de3ba7e338802f2ff53
timothy adigun <adigun.timothy@gmail.com>
2026-07-28 23:18:25 +0100

    recursion in python

--- playground/tailfac.py
+++ playground/tailfac.py
@@ -30,6 +30,13 @@ def tail_fac(n: int) -> int:
     return helper(n, 1)
 
 
+def fib(n: int) -> int:
+    nums = [0, 1]
+    for i in range(2, n + 1):
+        nums.append(nums[i - 1] + nums[i - 2])
+    return nums[n]
+
+
 def from_user(prompt: str) -> str:
     return input(prompt).strip()
 
@@ -43,8 +50,17 @@ def main() -> None:
         try:
             input = get_number(from_user("Enter a number:"))
             print(tail_fac(input))
+            if input == 0:
+                break
         except ValueError:
-            print("Invalid input. Please enter a number.")
+            print("Invalid input. Please enter a number. Enter 0 to exit.")
+
+    factorial_func = lambda x, f: f(x)
+    result = factorial_func(56, fac)
+    print(result)
+    #
+    # fibonacci
+    print(fib(560))
 
 
 if __name__ == "__main__":
