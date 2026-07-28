ec785af444585611abda0f550faedbd0d521f451
timothy adigun <adigun.timothy@gmail.com>
2026-07-28 21:54:51 +0100

    tail factorial using python

---
+++ playground/tailfac.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+
+def fac(n: int) -> int:
+    def helper(n: int, acc: int) -> int:
+        if n == 0:
+            return acc
+        return helper(n - 1, acc * n)
+
+    return helper(n, 1)
+
+
+def from_user(prompt: str) -> str:
+    return input(prompt).strip()
+
+
+def get_number(user_input: str) -> int:
+    return int(user_input)
+
+
+def main() -> None:
+    while True:
+        try:
+            input = get_number(from_user("Enter a number:"))
+            print(fac(input))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+
+if __name__ == "__main__":
+    main()
