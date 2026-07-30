e743ba7130d293ff68e9199c62b47e75c13ca87a
timothy adigun <adigun.timothy@gmail.com>
2026-07-30 23:56:58 +0100

    with in context manager

--- practiced/practicecontentmanager.py
+++ practiced/practicecontentmanager.py
@@ -1,8 +1,32 @@
 #!/usr/bin/env python3
 
+import datetime
+
+
+class MyLooger:
+    def __init__(self, filename: str):
+        self.filename = filename
+        self.file = None
+
+    def log(self, message: str) -> None:
+        if self.file is None:
+            raise ValueError("Log file is not open")
+        self.file.write(f"{datetime.datetime.now(tz=datetime.timezone.utc)} {message}")
+
+    def __enter__(self):
+        self.file = open(self.filename, "w")
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.file:
+            self.file.close()
+
+
 def main() -> None:
-    print("Start Here!")
+    with MyLooger("log.txt") as logger:
+        logger.log("Hello, World!")
+        logger.log("Howdy! Earth...")
+
 
 if __name__ == "__main__":
     main()
-  
